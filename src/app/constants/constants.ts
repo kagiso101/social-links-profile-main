3fb2b96059500aecf1dd65c10e4d18caa555466a
Kagiso <kagisoh@rain.co.za>
2025-02-06 23:59:09 +0200

    [scaffolding]

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,24 @@
+import { SocialLinks } from "./types";
+
+export const LinkInfo : SocialLinks[] = [
+    { linkName: 'Facebook', linkUrl: 'https://www.facebook.com/profile.php?id=100074329884922' },
+    { linkName: 'Instagram', linkUrl: 'https://www.instagram.com/kagiso.ha/' },
+    { linkName: 'LinkedIn', linkUrl: 'https://www.linkedin.com/in/kagiso-hadebe-611a9a192/' },
+    { linkName: 'Github', linkUrl: 'https://github.com/kagiso101' },
+  ]
+
+  export const AdviceContent = {
+    image: '../../assets/designs/active-states.jpg',
+    location: 'Cape Town, South Africa',
+    headline: 'Innovator ⚡ Coder 🎮 Strategist 🚀 Game Changer',
+    name: 'Kagiso Hadebe (KG)'
+  };
+
+
+
+  export interface AdviceObject {
+    slip: {
+      id: number;
+      advice: string;
+    };
+  }
